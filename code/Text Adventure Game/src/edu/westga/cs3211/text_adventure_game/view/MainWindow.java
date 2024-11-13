ff1a3b582166db49a1c8a3b5226d77b42ec4bff9
Jennifer Alvarez <ja00150@my.westga.edu>
2024-11-13 18:12:38 -0500

    Implemented UI(FXML and Codebehind)
    Added elements into the TextAdventure game FXML file and updated the codebehind for the new FXML elements

--- code/Text Adventure Game/src/edu/westga/cs3211/text_adventure_game/view/MainWindow.java
+++ code/Text Adventure Game/src/edu/westga/cs3211/text_adventure_game/view/MainWindow.java
@@ -1,5 +1,10 @@
 package edu.westga.cs3211.text_adventure_game.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
 /** Codebehind for the Main Window of the application.
  * 
  * @author CS 3211
@@ -7,4 +12,24 @@ package edu.westga.cs3211.text_adventure_game.view;
  */
 public class MainWindow {
 
+    @FXML
+    private TextArea actionDescriptionsTextArea;
+
+    @FXML
+    private ComboBox<?> actionsComboBox;
+
+    @FXML
+    private TextArea locationDescriptionTextArea;
+
+    @FXML
+    private Button takeActionButton;
+
+    @FXML
+    void initialize() {
+        assert this.actionDescriptionsTextArea != null : "fx:id=\"actionDescriptionsTextArea\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.actionsComboBox != null : "fx:id=\"actionsComboBox\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.locationDescriptionTextArea != null : "fx:id=\"locationDescriptionTextArea\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.takeActionButton != null : "fx:id=\"takeActionButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+
+    }
 }
